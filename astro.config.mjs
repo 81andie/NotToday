51e4c9acb1b3ae04bc1520cecf69b86fc9724673
81andie <133395577+81andie@users.noreply.github.com>
2026-01-20 22:44:02 +0100

    guardando

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel';
 
 
 import tailwind from "@astrojs/tailwind";
